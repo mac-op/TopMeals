6d64605ed27e99a931140ca56a076269071a5ab5
Andrew Samoil <aisamoil@ualberta.ca>
2022-09-29 19:20:24 -0600

    Added Top G test comment

--- app/src/main/java/com/example/topgmeals/MainActivity.java
+++ app/src/main/java/com/example/topgmeals/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Top G
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
