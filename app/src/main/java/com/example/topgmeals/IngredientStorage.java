6267f44410693598344a204f3225b0fb060a285c
Andrew Samoil <aisamoil@ualberta.ca>
2022-10-23 21:00:04 -0600

    Deleted Shapes. Added activites 2

---
+++ app/src/main/java/com/example/topgmeals/IngredientStorage.java
@@ -0,0 +1,14 @@
+package com.example.topgmeals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_storage);
+    }
+}
