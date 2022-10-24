606aa8c26409d90db1b840292a64afca325954b3
kgandhi31 <gandhi@ualberta.ca>
2022-10-23 23:23:16 -0600

    Connected app to Firebase.

--- app/src/main/java/com/example/topgmeals/MainActivity.java
+++ app/src/main/java/com/example/topgmeals/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Top G
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
