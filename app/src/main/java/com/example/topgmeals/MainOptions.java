750fb7208cc4a2e089104c423de1cea4845949fa
kgandhi31 <gandhi@ualberta.ca>
2022-10-24 23:59:37 -0600

    MainOptions Activity UI done.

--- app/src/main/java/com/example/topgmeals/MainOptions.java
+++ app/src/main/java/com/example/topgmeals/MainOptions.java
@@ -2,7 +2,10 @@ package com.example.topgmeals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainOptions extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainOptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_options);
+
+        Button btnIngredientStorage = (Button) findViewById(R.id.btnOptionIngredientStorage);
+        btnIngredientStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainOptions.this, IngredientStorage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnShoppingList = (Button) findViewById(R.id.btnOptionShoppingList);
+        btnShoppingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainOptions.this, ShoppingList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnMealPlanner = (Button) findViewById(R.id.btnOptionMealPlanner);
+        btnMealPlanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainOptions.this, MealPlan.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnRecipesBook = (Button) findViewById(R.id.btnOptionRecipesBook);
+        btnRecipesBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainOptions.this, Recipes.class);
+                startActivity(intent);
+            }
+        });
     }
 }
