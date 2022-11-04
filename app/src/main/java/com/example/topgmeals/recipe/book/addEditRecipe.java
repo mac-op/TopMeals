72d0b8ede656eed7bc61c5465a6c1fd64944bfdf
Almer Muneer <muneer1@ualberta.ca>
2022-11-04 13:41:17 -0600

    Cloned with Krish

--- app/src/main/java/com/example/topgmeals/recipe/book/addEditRecipe.java
+++ app/src/main/java/com/example/topgmeals/recipe/book/addEditRecipe.java
@@ -2,6 +2,7 @@ package com.example.topgmeals.recipe.book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.topgmeals.R;
+import com.example.topgmeals.recipe.book.Recipe;
+import com.example.topgmeals.recipe.book.RecipeBook;
 
 public class addEditRecipe extends AppCompatActivity {
 
@@ -18,33 +21,55 @@ public class addEditRecipe extends AppCompatActivity {
         setContentView(R.layout.add_recipee);
         addEditRecipe currentClass = addEditRecipe.this;
 
-        Button add_new=findViewById(R.id.add_new);
+
+
+
+
+
+        Button add_new=findViewById(R.id.add_recipe);
         add_new.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent_add = new Intent(currentClass, RecipeBook.class);
-                EditText title=(EditText) findViewById(R.id.title_editText);
-                EditText prep_time=(EditText) findViewById(R.id.prep_time_editText);
-                EditText serving=(EditText) findViewById(R.id.serving_editText);
-                EditText category=(EditText) findViewById(R.id.Category_editText);
-                EditText comments=(EditText) findViewById(R.id.Comments_editText);
+                EditText title = (EditText) findViewById(R.id.title_editText);
+                EditText prep_time = (EditText) findViewById(R.id.prep_time_editText);
+                EditText serving = (EditText) findViewById(R.id.serving_editText);
+                EditText category = (EditText) findViewById(R.id.Category_editText);
+                EditText comments = (EditText) findViewById(R.id.Comments_editText);
 
-                String title_text=title.getText().toString();
-                String prep_time_text=prep_time.getText().toString();
-                //Integer serving_text=Integer.parseInt(serving.getText().toString());
-                String category_text=category.getText().toString();
-                String comments_text=comments.getText().toString();
+                String title_text = title.getText().toString();
+                String prep_time_text = prep_time.getText().toString();
+                Integer serving_text = Integer.parseInt(serving.getText().toString());
+                String category_text = category.getText().toString();
+                String comments_text = comments.getText().toString();
 
-                System.out.println(title_text);
-                System.out.println(prep_time);
-                System.out.println('3');
+//                System.out.println(title_text);
+//                System.out.println(prep_time);
+//                System.out.println('3');
+//                Boolean flag = true;
+//                for (int i = 0; i < serving_text.toString().length(); i++) {
+//                    if (!Character.isDigit(serving_text.toString().charAt(i))) {
+//                        serving.setError("Enter valid servings number!");
+//                        flag = false;
+//                    }
+//                }
 
-
-                Recipe new_recipe =new Recipe(title_text,prep_time_text,3,category_text,comments_text);
-                intent_add.putExtra("NEW",new_recipe);
+                Recipe new_recipe = new Recipe(title_text, prep_time_text, serving_text, category_text, comments_text);
+                intent_add.putExtra("NEW", new_recipe);
                 startActivity(intent_add);
+
+
             }
         });
+
+        Button back = findViewById(R.id.back_button_recipe);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
 
 
 
