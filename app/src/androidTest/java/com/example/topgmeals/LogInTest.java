cc6206ffaff304fe03119390b947a364b20c4f02
mac-op <htta@ualberta.ca>
2022-11-27 21:09:35 -0700

    added tests for LogInTest

--- app/src/androidTest/java/com/example/topgmeals/LogInTest.java
+++ app/src/androidTest/java/com/example/topgmeals/LogInTest.java
@@ -4,6 +4,7 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 
 import android.app.Activity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -12,9 +13,11 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.example.topgmeals.login.MainActivity;
 import com.example.topgmeals.login.MainOptions;
+import com.example.topgmeals.login.MainRegisterUser;
 import com.robotium.solo.Solo;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,16 +54,36 @@ public class LogInTest {
      * Enter credentials to create an account. After that user will be taken to Main Options menu.
      */
     @Test
-    public void LogIn(){
-//        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-//        //Enter email
-//        solo.enterText((EditText) solo.getView(R.id.main_email), "testNewemail@gmail.com");
-//        //Enter password
-//        solo.enterText((EditText) solo.getView(R.id.main_password), "randompass");
-//        //Click on Create Account
-//        solo.clickOnButton("Sign In");
-//
-//        // Now in Main Options
-//        solo.assertCurrentActivity("Not in Main Options", MainOptions.class);
+    public void logInTest(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        //Enter email
+        solo.enterText((EditText) solo.getView(R.id.emailAddress), "testNewemail@gmail.com");
+        //Enter password
+        solo.enterText((EditText) solo.getView(R.id.password), "randompass");
+        //Click on Create Account
+        solo.clickOnButton("LOGIN");
+
+        // Now in Main Options
+        solo.assertCurrentActivity("Not in Main Options", MainOptions.class);
+    }
+
+    /**
+     * This method tests the ability to create a new account. The account email and password should
+     * be changed every time before this test is done.
+     */
+    @Test
+    @Ignore("This test has been confirmed to work. Please change the email and password to run again")
+    public void registerTest(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView((TextView) solo.getView(R.id.register));
+        solo.assertCurrentActivity("Wrong Activity", MainRegisterUser.class);
+
+        solo.enterText((EditText) solo.getView(R.id.registerName), "Mock Name");
+        solo.enterText((EditText) solo.getView(R.id.registerEmailAddress), "mail@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.registerPassword), "123456");
+        solo.clickOnButton("CREATE ACCOUNT");
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
     }
 }
